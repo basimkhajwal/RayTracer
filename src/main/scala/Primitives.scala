9300c076c185575b79aaace32f54b89d6c75e1c0
Basim Khajwal <basimkhajwal@gmail.com>
2016-12-17 13:24:48 +0000

    Fixed error in the RGB generation and added spectrum multiplying. Also, included a colour term in the Hit class, to be replaced by BDSF soon

--- src/main/scala/Primitives.scala
+++ src/main/scala/Primitives.scala
@@ -10,10 +10,12 @@ case class Ray(val start: Vec3, val dir: Vec3) {
 }
 
 case class Spectrum(val r: Double, val g: Double, val b: Double) {
-  def toRGBInt: Int = (r*255.0).toInt + ((g*255.0).toInt << 8) + ((b*255.0).toInt << 16)
+  def toRGBInt: Int = (b*255.0).toInt + ((g*255.0).toInt << 8) + ((r*255.0).toInt << 16)
 
   def +(that: Spectrum): Spectrum = Spectrum(r + that.r, g + that.g, b + that.b)
   def *(sf: Double): Spectrum = Spectrum(r * sf, g * sf, b * sf)
+  def *(that: Spectrum): Spectrum = Spectrum(r * that.r, g * that.g, b * that.b)
+
   def clamp: Spectrum = Spectrum(Math.min(r, 1), Math.min(g, 1), Math.min(b, 1))
 }
 object Spectrum {
@@ -28,4 +30,4 @@ case class PointLight(val pos: Vec3, val colour: Spectrum)
 
 trait Intersection
 object Miss extends Intersection
-case class Hit(val t: Double, val point: Vec3, val normal: Vec3) extends Intersection
+case class Hit(val t: Double, val point: Vec3, val normal: Vec3, val colour: Spectrum) extends Intersection
