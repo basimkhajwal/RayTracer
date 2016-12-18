8111b20a38e638db41acbf6ad6a7ec136e7c2e4d
Basim Khajwal <basimkhajwal@gmail.com>
2016-12-18 22:27:37 +0000

    Made code even neater by moving impure IO out into the Main file

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,73 @@
+import java.awt.{Dimension, Graphics}
+import java.awt.image.BufferedImage
+import java.io.File
+import java.nio.file.{Files, Paths}
+import javax.imageio.ImageIO
+import javax.swing.{JFrame, JPanel}
+
+/**
+  * Created by Basim on 18/12/2016.
+  */
+object Main {
+
+  val lights = PointLight(Vec3(0, 0.5, -0.5), Spectrum.WHITE*0.6) ::
+    PointLight(Vec3(-1, -0.5, 1), Spectrum.WHITE * 0.6) ::
+    Nil
+
+  val bigWidth: Double = 1e5
+  val room:Double = 5
+  val spheres =
+    Sphere(1, Vec3(3, 0, 4), Spectrum(0.8, 0.8, 0.8)) ::
+      Sphere(0.5, Vec3(1, 0, 2), Spectrum.WHITE) ::
+      Sphere(bigWidth, Vec3(-room-bigWidth, 0, 0), Spectrum(0.2, 0.3, 0.8)) :: // L
+      Sphere(bigWidth, Vec3(room+bigWidth, 0, 0), Spectrum(0.2, 0.8, 0.3)) :: // R
+      Sphere(bigWidth, Vec3(0, 0, room+bigWidth), Spectrum(0.7, 0.2, 0.3)) :: // F
+      Sphere(bigWidth, Vec3(0, 0, -room-bigWidth), Spectrum.WHITE) :: // B
+      Sphere(bigWidth, Vec3(0, room+bigWidth, 0), Spectrum(0.4, 0.4, 0.4)) :: // U
+      Sphere(bigWidth, Vec3(0, -room-bigWidth, 0), Spectrum.WHITE * 0.2) :: // D
+      Nil
+
+  def main(args: Array[String]) = draw
+
+  def render: BufferedImage = {
+    new Renderer(new RenderOpts {
+      override val imgHeight: Int = 600
+      override val scene: Scene = new Scene(lights, spheres)
+      override val imgWidth: Int = 800
+    }).render
+  }
+
+  def bench: Unit = {
+    val before = System.currentTimeMillis
+    render
+    println("Time taken: " + (System.currentTimeMillis - before))
+  }
+
+  def draw: Unit = {
+    val frame = new JFrame("Sphere Render Test")
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+    frame.setResizable(false)
+    frame.add(new CustomRenderer(render))
+    frame.pack
+    frame.setVisible(true)
+  }
+
+  def save: Unit = {
+    def innerFile(n: Int) = "progress/" + n + ".png"
+    def exists(n: Int) = Files.exists(Paths.get(innerFile(n)))
+    def saveImage(n: Int, img: BufferedImage) = ImageIO.write(img, "png", new File(innerFile(n)))
+
+    def findFirst(n: Int): Unit = if (exists(n)) findFirst(n+1) else saveImage(n, render)
+    findFirst(0)
+  }
+
+  class CustomRenderer(val img: BufferedImage) extends JPanel {
+
+    override def getPreferredSize: Dimension = new Dimension(img.getWidth, img.getHeight)
+
+    override def paint(g: Graphics): Unit = {
+      super.paint(g)
+      g.drawImage(img, 0, 0, img.getWidth, img.getHeight, null)
+    }
+  }
+}
--- src/main/scala/Renderer.scala
+++ src/main/scala/Renderer.scala
@@ -1,103 +1,63 @@
 /**
   * Created by Basim on 15/12/2016.
   */
-import java.awt.{Dimension, Graphics}
 import java.awt.image.BufferedImage
-import java.io.File
-import java.nio.file.{Files, Paths}
-import javax.imageio.ImageIO
-import javax.swing.{JFrame, JPanel}
 
-object Renderer {
+trait RenderOpts {
+  val maxRayDepth: Int = 4
+  val pixelSampleCount: Int = 2
 
-  val lights = PointLight(Vec3(0, 0.5, -0.5), Spectrum.WHITE*0.6) ::
-    PointLight(Vec3(-1, -0.5, 1), Spectrum.WHITE * 0.6) ::
-    Nil
+  val cameraPos: Vec3 = Vec3(0, 0, -2)
+  val cameraAngle: Vec3 = Vec3.ZERO
 
-  val bigWidth: Double = 1e5
-  val room:Double = 5
-  val spheres =
-    Sphere(1, Vec3(3, 0, 4), Spectrum(0.8, 0.8, 0.8)) ::
-      //Sphere(0.5, Vec3(1, 0, 0)) ::
-      Sphere(bigWidth, Vec3(-room-bigWidth, 0, 0), Spectrum(0.2, 0.3, 0.8)) :: // L
-      Sphere(bigWidth, Vec3(room+bigWidth, 0, 0), Spectrum(0.2, 0.8, 0.3)) :: // R
-      Sphere(bigWidth, Vec3(0, 0, room+bigWidth), Spectrum(0.7, 0.2, 0.3)) :: // F
-      Sphere(bigWidth, Vec3(0, 0, -room-bigWidth), Spectrum.WHITE) :: // B
-      Sphere(bigWidth, Vec3(0, room+bigWidth, 0), Spectrum(0.4, 0.4, 0.4)) :: // U
-      Sphere(bigWidth, Vec3(0, -room-bigWidth, 0), Spectrum.WHITE * 0.2) :: // D
-      Nil
+  val imgWidth: Int
+  val imgHeight: Int
 
-  val scene = new Scene(lights, spheres)
+  val scene: Scene
+}
+
+class Renderer(options: RenderOpts) {
+
+  val scene: Scene = options.scene
 
   def traceRay(ray: Ray, depth: Int): Spectrum = {
     scene intersect ray match {
       case Miss => Spectrum.BLACK
       case Hit(_, p, n, c) => {
 
-        val directLight = lights.filter(l => {
-          val lightT = l.pos.dist(p)
-          scene intersect Ray(p, (l.pos - p).nor) match {
-            case Miss => true
-            case Hit(newT, _, _, _) => newT > lightT
-          }
-        }).map(l => l.colour * (l.pos - p).nor.dot(n)).sum
+        val directLight =
+          scene.lights.filter(l => {
+            val lightT = l.pos.dist(p)
+            scene intersect Ray(p, (l.pos - p).nor) match {
+              case Miss => true
+              case Hit(newT, _, _, _) => newT > lightT
+            }
+          })
+          .map(l => l.colour * (l.pos - p).nor.dot(n))
+          .foldLeft(Spectrum.BLACK)(_ + _)
 
-        if (depth == 4) c * directLight
+        if (depth == options.maxRayDepth) c * directLight
         else c * (directLight + 0.2 * traceRay(Ray(p, ray reflect n), depth + 1))
       }
     }
   }
 
-  def render: BufferedImage = {
-    val sz:Int = 600
-    val hsz: Double = sz/2.0
-    val img = new BufferedImage(sz, sz, BufferedImage.TYPE_INT_RGB)
+  lazy val render: BufferedImage = {
+    val hw = options.imgWidth / 2.0
+    val hh = options.imgHeight / 2.0
+    val invSampleCount = 1.0 / options.pixelSampleCount
+    val img = new BufferedImage(options.imgWidth, options.imgHeight, BufferedImage.TYPE_INT_RGB)
 
-    val start = Vec3(0, 0, -2) //'(-2, 0, -1)
+    for (y <- 0 until options.imgHeight; x <- 0 until options.imgWidth) {
 
-    for (y <- 0 until sz; x <- 0 until sz) {
-      val rayDir = Vec3((x-hsz)/hsz, (y-hsz)/hsz, 1).nor
-      val lum = traceRay(Ray(start, rayDir), 0).clamp
+      val rayDir = Vec3((x-hw)/hh, (y-hh)/hh , 1).nor
+      val lum = (1 to options.pixelSampleCount)
+        .map(_ => traceRay(Ray(options.cameraPos, rayDir), 0))
+        .reduce(_ + _) * invSampleCount
 
-      img.setRGB(x, sz-y-1, lum.toRGBInt)
+      img.setRGB(x, options.imgHeight-y-1, lum.clamp.toRGBInt)
     }
 
     img
-  }
-
-  def main(args: Array[String]) = draw
-
-  def bench: Unit = {
-    val before = System.currentTimeMillis
-    render
-    println("Time taken: " + (System.currentTimeMillis - before))
-  }
-
-  def draw: Unit = {
-    val frame = new JFrame("Sphere Render Test")
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
-    frame.setResizable(false)
-    frame.add(new CustomRenderer(render))
-    frame.pack
-    frame.setVisible(true)
-  }
-
-  def save: Unit = {
-    def innerFile(n: Int) = "progress/" + n + ".png"
-    def exists(n: Int) = Files.exists(Paths.get(innerFile(n)))
-    def saveImage(n: Int, img: BufferedImage) = ImageIO.write(img, "png", new File(innerFile(n)))
-
-    def findFirst(n: Int): Unit = if (exists(n)) findFirst(n+1) else saveImage(n, render)
-    findFirst(0)
-  }
-
-  class CustomRenderer(val img: BufferedImage) extends JPanel {
-
-    override def getPreferredSize: Dimension = new Dimension(img.getWidth, img.getHeight)
-
-    override def paint(g: Graphics): Unit = {
-      super.paint(g)
-      g.drawImage(img, 0, 0, img.getWidth, img.getHeight, null)
-    }
   }
 }
