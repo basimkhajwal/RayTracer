466c4c4198c32ea05c13c8c127f8aba8a2ec0e18
Basim Khajwal <basimkhajwal@gmail.com>
2017-01-10 22:59:18 +0000

    Added code for union with vectors

--- src/main/scala/raytracer/math/BBox.scala
+++ src/main/scala/raytracer/math/BBox.scala
@@ -13,24 +13,34 @@ class BBox(val pMin: Vec3, val pMax: Vec3) {
   @inline
   private final def inRange(v: Double, a: Double, b: Double): Boolean = v >= a && v <= b
 
+  def apply(index: Int): Vec3 = {
+    if (index == 0) pMin
+    else if (index == 1) Vec3(pMin.x, pMin.y, pMax.z)
+    else if (index == 2) Vec3(pMin.x, pMax.y, pMin.z)
+    else if (index == 3) Vec3(pMin.x, pMax.y, pMax.z)
+    else if (index == 4) Vec3(pMax.x, pMin.y, pMin.z)
+    else if (index == 5) Vec3(pMax.x, pMin.y, pMax.z)
+    else if (index == 6) Vec3(pMax.x, pMax.y, pMin.z)
+    else pMax
+  }
+
   def contains(point: Vec3): Boolean = {
     inRange(point.x, pMin.x, pMax.x) &&
     inRange(point.y, pMin.y, pMax.y) &&
     inRange(point.z, pMin.z, pMax.z)
   }
 
+  def union(p: Vec3): BBox = {
+    new BBox (
+      Vec3( min(pMin.x, p.x), min(pMin.y, p.y), min(pMin.z, p.z) ),
+      Vec3( max(pMax.x, p.x), max(pMax.y, p.y), max(pMax.z, p.z) )
+    )
+  }
+
   def union(that: BBox): BBox = {
     new BBox (
-      Vec3(
-        min(pMin.x, that.pMin.x),
-        min(pMin.y, that.pMin.y),
-        min(pMin.z, that.pMin.z)
-      ),
-      Vec3(
-        max(pMax.x, that.pMax.x),
-        max(pMax.y, that.pMax.y),
-        max(pMax.z, that.pMax.z)
-      )
+      Vec3( min(pMin.x, that.pMin.x), min(pMin.y, that.pMin.y), min(pMin.z, that.pMin.z) ),
+      Vec3( max(pMax.x, that.pMax.x), max(pMax.y, that.pMax.y), max(pMax.z, that.pMax.z) )
     )
   }
 }
