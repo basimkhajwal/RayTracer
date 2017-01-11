6776c55dd4bb8f73b76f457d0122b35d00904261
Basim Khajwal <basimkhajwal@gmail.com>
2017-01-11 13:28:11 +0000

    Made bounding box a case class using Points rather than Vec3

--- src/main/scala/raytracer/math/BBox.scala
+++ src/main/scala/raytracer/math/BBox.scala
@@ -5,7 +5,7 @@ import scala.math._
 /**
   * Created by Basim on 09/01/2017.
   */
-class BBox(val pMin: Vec3, val pMax: Vec3) {
+case class BBox(pMin: Point, pMax: Point) {
   require(pMin.x <= pMax.x)
   require(pMin.y <= pMax.y)
   require(pMin.z <= pMax.z)
@@ -13,34 +13,34 @@ class BBox(val pMin: Vec3, val pMax: Vec3) {
   @inline
   private final def inRange(v: Double, a: Double, b: Double): Boolean = v >= a && v <= b
 
-  def apply(index: Int): Vec3 = {
+  def apply(index: Int): Point = {
     if (index == 0) pMin
-    else if (index == 1) Vec3(pMin.x, pMin.y, pMax.z)
-    else if (index == 2) Vec3(pMin.x, pMax.y, pMin.z)
-    else if (index == 3) Vec3(pMin.x, pMax.y, pMax.z)
-    else if (index == 4) Vec3(pMax.x, pMin.y, pMin.z)
-    else if (index == 5) Vec3(pMax.x, pMin.y, pMax.z)
-    else if (index == 6) Vec3(pMax.x, pMax.y, pMin.z)
+    else if (index == 1) Point(pMin.x, pMin.y, pMax.z)
+    else if (index == 2) Point(pMin.x, pMax.y, pMin.z)
+    else if (index == 3) Point(pMin.x, pMax.y, pMax.z)
+    else if (index == 4) Point(pMax.x, pMin.y, pMin.z)
+    else if (index == 5) Point(pMax.x, pMin.y, pMax.z)
+    else if (index == 6) Point(pMax.x, pMax.y, pMin.z)
     else pMax
   }
 
-  def contains(point: Vec3): Boolean = {
+  def contains(point: Point): Boolean = {
     inRange(point.x, pMin.x, pMax.x) &&
     inRange(point.y, pMin.y, pMax.y) &&
     inRange(point.z, pMin.z, pMax.z)
   }
 
-  def union(p: Vec3): BBox = {
-    new BBox (
-      Vec3( min(pMin.x, p.x), min(pMin.y, p.y), min(pMin.z, p.z) ),
-      Vec3( max(pMax.x, p.x), max(pMax.y, p.y), max(pMax.z, p.z) )
+  def union(p: Point): BBox = {
+    BBox(
+      Point( min(pMin.x, p.x), min(pMin.y, p.y), min(pMin.z, p.z) ),
+      Point( max(pMax.x, p.x), max(pMax.y, p.y), max(pMax.z, p.z) )
     )
   }
 
   def union(that: BBox): BBox = {
-    new BBox (
-      Vec3( min(pMin.x, that.pMin.x), min(pMin.y, that.pMin.y), min(pMin.z, that.pMin.z) ),
-      Vec3( max(pMax.x, that.pMax.x), max(pMax.y, that.pMax.y), max(pMax.z, that.pMax.z) )
+    BBox(
+      Point( min(pMin.x, that.pMin.x), min(pMin.y, that.pMin.y), min(pMin.z, that.pMin.z) ),
+      Point( max(pMax.x, that.pMax.x), max(pMax.y, that.pMax.y), max(pMax.z, that.pMax.z) )
     )
   }
 }
