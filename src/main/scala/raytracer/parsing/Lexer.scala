905f283f7910ce27bf198056357e4d750994d0c4
Basim Khajwal <basimkhajwal@gmail.com>
2017-01-18 23:19:45 +0000

    Used the standard library tokeniser and made it more useful

--- src/main/scala/raytracer/parsing/Lexer.scala
+++ src/main/scala/raytracer/parsing/Lexer.scala
@@ -1,6 +1,6 @@
 package raytracer.parsing
 
-import scala.collection.mutable.ListBuffer
+import java.io.{FileReader, StreamTokenizer}
 
 /**
   *
@@ -9,60 +9,22 @@ import scala.collection.mutable.ListBuffer
   *
   * Created by Basim on 12/01/2017.
   */
-class Lexer(val input: String) {
+class Lexer(val fileName: String) {
 
-  type Token = String
+  private val fin = new FileReader(fileName)
+  private val tokenize = new StreamTokenizer(fin)
 
-  def tokenise: List[Token] = {
+  tokenize.commentChar('#')
 
-    var i = 0
-    var currentTokenStart = -1
-    var isComment = false
-    var isString = false
-    val ls: ListBuffer[Token] = new ListBuffer[Token]
+  def currentLine: Int = tokenize.lineno
 
-    while (i < input.length) {
-      val c = input(i)
-
-      if (isComment) {
-        if (c == '\n') isComment = false
-      }
-      else if (c == '#' || (!isString && c.isWhitespace)) {
-        if (currentTokenStart != -1) {
-          ls append input.substring(currentTokenStart, i)
-          currentTokenStart = -1
-        }
-        if (c == '#') isComment = true
-      }
-      else if (c == '[' || c == ']') {
-
-        if (currentTokenStart != -1) {
-          ls append input.substring(currentTokenStart, i)
-        }
-
-        ls append c.toString
-        currentTokenStart = -1
-      }
-      else if (c == '"') {
-        if (isString) {
-          ls append input.substring(currentTokenStart, i+1)
-          currentTokenStart = -1
-          isString = false
-        } else {
-          isString = true
-          currentTokenStart = i
-        }
-      }
-      else if (currentTokenStart == -1) {
-        currentTokenStart = i
-      }
-
-      i += 1
-    }
-
-    if (currentTokenStart != -1) ls append input.substring(currentTokenStart)
-    ls.toList
+  def nextToken(): Option[String] = tokenize.nextToken match {
+    case StreamTokenizer.TT_WORD => Some(tokenize.sval)
+    case '"' => Some(tokenize.sval)
+    case StreamTokenizer.TT_NUMBER => Some(tokenize.nval.toString)
+    case c => Some(c.toChar.toString)
+    case StreamTokenizer.TT_EOL => nextToken
+    case StreamTokenizer.TT_EOF => None
   }
-
 
 }
