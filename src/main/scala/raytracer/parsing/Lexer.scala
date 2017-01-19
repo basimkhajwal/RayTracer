42719aa21d1a8f01370e41cfe99d2dbd1f74b86d
Basim Khajwal <basimkhajwal@gmail.com>
2017-01-19 19:21:38 +0000

    Added peeking and made the interface more robust

--- src/main/scala/raytracer/parsing/Lexer.scala
+++ src/main/scala/raytracer/parsing/Lexer.scala
@@ -2,6 +2,9 @@ package raytracer.parsing
 
 import java.io.{FileReader, StreamTokenizer}
 
+import scala.collection.mutable.Queue
+import scala.collection.mutable.ListBuffer
+
 /**
   *
   * Tokenise the input and perform lexical analysis
@@ -11,20 +14,49 @@ import java.io.{FileReader, StreamTokenizer}
   */
 class Lexer(val fileName: String) {
 
-  private val fin = new FileReader(fileName)
-  private val tokenize = new StreamTokenizer(fin)
+  private val inputReader = new FileReader(fileName)
+  private val tokenize = new StreamTokenizer(inputReader)
+  private val peekedTokens: Queue[String] = Queue()
+
+  private var finished = false
 
   tokenize.commentChar('#')
 
   def currentLine: Int = tokenize.lineno
 
-  def nextToken(): Option[String] = tokenize.nextToken match {
+  private def getNextToken(): Option[String] = tokenize.nextToken match {
     case StreamTokenizer.TT_WORD => Some(tokenize.sval)
     case '"' => Some(tokenize.sval)
     case StreamTokenizer.TT_NUMBER => Some(tokenize.nval.toString)
     case c => Some(c.toChar.toString)
-    case StreamTokenizer.TT_EOL => nextToken
-    case StreamTokenizer.TT_EOF => None
+    case StreamTokenizer.TT_EOL => getNextToken
+
+    case StreamTokenizer.TT_EOF => {
+      finished = true
+      close()
+      None
+    }
   }
 
+  def next(): Option[String] = {
+    if (finished) None
+    else if (peekedTokens.isEmpty) getNextToken
+    else Some(peekedTokens.dequeue)
+  }
+
+  def peek(i: Int = 0): Option[String] = {
+    if (finished) None
+    else {
+      while (peekedTokens.size <= i) {
+        getNextToken match {
+          case None => return None
+          case Some(s) => peekedTokens enqueue s
+        }
+      }
+
+      Some(peekedTokens(i))
+    }
+  }
+
+  def close(): Unit = inputReader.close()
 }
