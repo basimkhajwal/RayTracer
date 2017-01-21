4f8428f25c68a2fa4774c19b9c8e8db552d6cfc5
Basim Khajwal <basimkhajwal@gmail.com>
2017-01-21 23:08:04 +0000

    Did a whole load of parsing, no other way to describe it

--- src/main/scala/raytracer/parsing/SceneParser.scala
+++ src/main/scala/raytracer/parsing/SceneParser.scala
@@ -1,56 +1,96 @@
 package raytracer.parsing
 
-import java.util.Stack
-
-import raytracer.math.{Point, Vec3}
+import raytracer.math.{Point, Transform, Vec3}
 
 import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
+import scala.reflect.ClassTag
+import scala.util.{Success, Try}
 
 /**
   * Created by Basim on 17/01/2017.
   */
 class SceneParser(sceneFile: String) {
 
-  val lexerStack: Stack[Lexer] = new Stack[Lexer]()
-  lexerStack push new Lexer(sceneFile)
+  var lexerStack: List[Lexer] = new Lexer(sceneFile) :: Nil
+
+  var transformStack: List[Transform] = Transform.identity :: Nil
 
   @inline
-  private def tokens = lexerStack peek
+  private def tokens = lexerStack head
+
+  @inline
+  private def ctm: Transform = transformStack head
+
+  private def applyTransform(t: Transform): Unit = setTransform(transformStack.head * t)
+
+  private def setTransform(t: Transform): Unit = transformStack = t :: transformStack.tail
+
+  private def pushTransform: Unit = transformStack ::= transformStack.head
+
+  private def popTransform: Unit = transformStack = transformStack.tail
 
   private def nextToken(): Option[String] = {
     if (lexerStack isEmpty) None
     else {
-      val t = tokens next
-      if (t.isDefined) t
-      else {
-        lexerStack pop()
-        nextToken()
+      tokens next match {
+        case None => {
+          lexerStack = lexerStack.tail
+          nextToken()
+        }
+        case t => t
       }
     }
   }
 
+  private def getTokens(n: Int): List[String] = {
+    var i = 0
+    val tk = ListBuffer[String]()
+    while (i < n) {
+      nextToken match {
+        case Some(t) => tk append t
+        case None => throwError(s"Error expected $n tokens but only got $i")
+      }
+      i += 1
+    }
+    tk toList
+  }
+
+  private def getNumbers(n: Int): List[Double] = {
+    var i = 0
+    val ns = ListBuffer[Double]()
+    while (i < n) {
+      nextToken match {
+        case Some(t) => Try(ns append t.toDouble).getOrElse(throwError(s"Invalid numerical value $t"))
+        case None => throwError(s"Error expected $n tokens but only got $i")
+      }
+      i += 1
+    }
+    ns toList
+  }
+
   private def throwError(msg: String): Nothing = {
-    val currentLexer = lexerStack peek
+    val currentLexer = lexerStack.head
     throw new RuntimeException(
       "Error parsing " + currentLexer.fileName + " at line " + currentLexer.currentLine
-      + "\n:" + msg
+      + ":\t" + msg
     )
   }
 
   sealed trait ParamT
-  case class PFloat(x: Double) extends ParamT
-  case class PInteger(x: Int) extends ParamT
-  case class PString(str: String) extends ParamT
-  case class PBool(b: Boolean) extends ParamT
-  case class PVector(v: Vec3) extends ParamT
-  case class PPoint(p: Point) extends ParamT
+  case class PFloat(x: Array[Double]) extends ParamT
+  case class PInteger(x: Array[Int]) extends ParamT
+  case class PString(str: Array[String]) extends ParamT
+  case class PBool(b: Array[Boolean]) extends ParamT
+  case class PVector(v: Array[Vec3]) extends ParamT
+  case class PPoint(p: Array[Point]) extends ParamT
 
   type Params = Map[String, ParamT]
 
   private val validParameters = List("float", "integer", "string", "bool", "vector", "point", "rgb")
 
   private def isValidHeader(paramHeader: String): Option[(String, String)] = {
-    val splitParts = paramHeader split(' ')
+    val splitParts = paramHeader.split(' ')
     if (splitParts.length != 2) None
     else {
       val paramType = splitParts(0).toLowerCase.trim
@@ -61,11 +101,62 @@ class SceneParser(sceneFile: String) {
     }
   }
 
-  private def parseParam(paramType: String): ParamT = paramType match {
-    case "float" => {
+  private def parseParam(paramType: String, paramValues: ListBuffer[String]): ParamT = {
 
+    def checkedMap[T:ClassTag](err: String => String, f: String => T): Array[T] = {
+      paramValues.map(value => Try(f(value)) match {
+        case Success(out: T) => out
+        case _ => throwError(err(value))
+      }).toArray[T]
     }
-    case _ => throwError("Unimplmented type " + paramType)
+
+    paramType match {
+      case "float" => PFloat(checkedMap(_ + " is not a valid float", _.toDouble))
+      case "integer" => PInteger(checkedMap(_ + " is not a valid integer", _.toDouble.toInt))
+      case "string" => PString(paramValues toArray)
+
+      case "bool" => PBool(checkedMap(_ + " is not a valid boolean", _ match {
+        case "true" => true
+        case "false" => false
+        case _ => ???
+      }))
+
+      case "vector" => {
+        val elements = checkedMap(_ + "is not a valid vector part", _.toDouble)
+        if (elements.size % 3 != 0) throwError("Invalid parts, 3 doubles per vector required")
+
+        PVector(elements grouped(3) map(p => Vec3(p(0), p(1), p(2))) toArray)
+      }
+
+      case "point" => {
+        val elements = checkedMap(_ + "is not a valid point part", _.toDouble)
+        if (elements.size % 3 != 0) throwError("Invalid parts, 3 doubles per point required")
+
+        PPoint(elements grouped(3) map(p => Point(p(0), p(1), p(2))) toArray)
+      }
+
+      case _ => throwError("Unimplemented type " + paramType)
+    }
+  }
+
+  private def getParamValues: ListBuffer[String] = {
+    nextToken() match {
+      case Some("[") => getParamValues(ListBuffer())
+      case Some(single) => ListBuffer(single)
+      case _ => throwError("Parameter list must begin with [")
+    }
+  }
+
+  @tailrec
+  private def getParamValues(acc: ListBuffer[String]): ListBuffer[String] = {
+    nextToken() match {
+      case None => throwError("Parameter list must end in a ]")
+      case Some("]")  => acc
+      case Some(token) => {
+        acc append token
+        getParamValues(acc)
+      }
+    }
   }
 
   @tailrec
@@ -74,8 +165,72 @@ class SceneParser(sceneFile: String) {
       case None => acc
       case Some(paramHeader) => isValidHeader(paramHeader) match {
         case None => acc
-        case Some(param) => parseParams(acc + param)
+        case Some((pType, pName)) => {
+          nextToken()
+          parseParams(acc +
+            (pName -> parseParam(pType, getParamValues)))
+        }
       }
     }
+  }
+
+  final def parse: Unit = {
+    var t: Option[String] = None
+    while ({
+      t = nextToken()
+      t.isDefined
+    }) {
+      t.get match {
+        case "WorldBegin" => parseWorld
+        case _ =>
+      }
+    }
+  }
+
+  final def parseWorld: Unit = {
+    var t: Option[String] = None
+    var done = false
+
+    while ({
+    t = nextToken()
+    !done && t.isDefined
+    }) t.get match {
+
+      case "WorldEnd" => done = true
+
+      case "Shape" => {
+        val shapeType = nextToken().getOrElse(throwError("Shape type not specified"))
+        parseShape(shapeType, parseParams())
+      }
+
+      case token if parseTransform(token) =>
+
+      case token => throwError(s"Token $token not recognised")
+    }
+  }
+
+  private def parseShape(shape: String, params: Params): Unit = shape match {
+
+    case "sphere" => {
+
+    }
+
+    case _ => throwError("Shape type " + shape + " not implemented")
+  }
+
+  def parseTransform(token: String): Boolean = token match {
+
+    case "Identity" => {
+      setTransform(Transform.identity)
+      true
+    }
+
+    case "Translate" => {
+      val ps = getNumbers(3)
+      applyTransform(Transform.translate(ps(0), ps(1), ps(2)))
+      true
+    }
+
+    case _ => false
   }
 }
