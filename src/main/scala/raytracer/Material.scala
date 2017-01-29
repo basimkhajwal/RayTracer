a5c64beacb0302ce860a50c8cb6782939c3a9d40
Basim Khajwal <basimkhajwal@gmail.com>
2017-01-29 13:43:46 +0000

    Started some kind of primitive definition system

---
+++ src/main/scala/raytracer/Material.scala
@@ -0,0 +1,8 @@
+package raytracer
+
+/**
+  * Created by Basim on 28/01/2017.
+  */
+class Material {
+
+}
---
+++ src/main/scala/raytracer/primitives/Primitive.scala
@@ -0,0 +1,10 @@
+package raytracer.primitives
+
+import raytracer.shapes.Shape
+
+/**
+  * Created by Basim on 28/01/2017.
+  */
+class Primitive(val shape: Shape) {
+
+}
