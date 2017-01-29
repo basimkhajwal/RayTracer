6e816b4c12e909e83c4c0052a5520bba743688e3
Basim Khajwal <basimkhajwal@gmail.com>
2017-01-29 13:43:13 +0000

    Removed old code

--- src/main/scala/raytracer/Renderer.scala
+++ src/main/scala/raytracer/Renderer.scala
@@ -27,24 +27,11 @@ trait RenderOpts {
 class Renderer(options: RenderOpts) {
 
   def render: BufferedImage = {
-    val hw = options.imgWidth / 2.0
-    val hh = options.imgHeight / 2.0
     val cam = new PerspectiveCamera(options.cameraToWorld,
-      (-8, 8, -6, 6), options.imgWidth, options.imgHeight, 0.1, 100, 30)
-    val dx = 1.0 / hw
-    val dy = 1.0 / hh
-    val invSampleCount = 1.0 / options.pixelSampleCount
+      (-1, 1, -1, 1), options.imgWidth, options.imgHeight, 0.1, 100, 160)
     val img = new BufferedImage(options.imgWidth, options.imgHeight, BufferedImage.TYPE_INT_RGB)
 
-    def randomVariation(dir: Vec3): Vec3 = dir + Vec3(Random.nextDouble()*dx, Random.nextDouble()*dy, 0)
-
     for (y <- 0 until options.imgHeight; x <- 0 until options.imgWidth) {
-      val rayDir = Vec3((x-hw)/hh, (y-hh)/hh , 1)
-      //val lum = (1 to options.pixelSampleCount)
-       // .map(_ =>
-          //options.integrator.traceRay(Ray(options.cameraPos, randomVariation(rayDir).nor))(options) )
-        //.reduce(_ + _) * invSampleCount
-      //val lum = options.integrator.traceRay(Ray(Point(0,0,-2), randomVariation(rayDir).nor))(options)
       val lum = options.integrator.traceRay(cam.generateRay(x, y))(options)
       img.setRGB(x, y, lum.clamp.toRGBInt)
     }
