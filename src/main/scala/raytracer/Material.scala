53c017d6d6c158ce8231ed0df7da734c80817cb0
Basim Khajwal <basimkhajwal@gmail.com>
2017-02-10 20:36:46 +0000

    Added base material interface

--- src/main/scala/raytracer/Material.scala
+++ src/main/scala/raytracer/Material.scala
@@ -1,8 +1,23 @@
 package raytracer
 
+import raytracer.shapes.DifferentialGeometry
+
 /**
   * Created by Basim on 28/01/2017.
   */
-class Material {
+trait Material {
+  def getBSDF(dgGeom: DifferentialGeometry, dgShading: DifferentialGeometry): BSDF
+}
 
+class MatteMaterial (
+  kd: Texture[Spectrum]
+  /* sig: Texture[Float] assumed to be 0 for now,
+   * bump: Texture[Float] also assumed to be 0 */
+) extends Material {
+
+  override def getBSDF(dgGeom: DifferentialGeometry, dgShading: DifferentialGeometry): BSDF = {
+    val bsdf = new BSDF(dgGeom, dgGeom.nn)
+    bsdf.add(new Lambertian(kd(dgGeom)))
+    bsdf
+  }
 }
