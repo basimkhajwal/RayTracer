253c4db53aad5d280315376b479e338472c9c50d
Basim Khajwal <basimkhajwal@gmail.com>
2017-02-19 11:21:25 +0000

    Changed point light definition

--- src/main/scala/raytracer/PointLight.scala
+++
@@ -1,8 +0,0 @@
-package raytracer
-
-import raytracer.math.{Point, Vec3}
-
-/**
-  * Created by Basim on 15/12/2016.
-  */
-case class PointLight(val pos: Point, val colour: Spectrum)
---
+++ src/main/scala/raytracer/lights/PointLight.scala
@@ -0,0 +1,18 @@
+package raytracer.lights
+import raytracer.Spectrum
+import raytracer.math.{Point, Transform, Vec3}
+
+/**
+  * Created by Basim on 18/02/2017.
+  */
+case class PointLight(lightToWorld: Transform, intensity: Spectrum) extends Light {
+
+  val lightPoint = lightToWorld(Point.ZERO)
+
+  override def sample(point: Point): (Spectrum, Vec3) = {
+
+    val lightVec = lightPoint - point
+
+    (intensity * (1/lightVec.mag2), lightVec.nor)
+  }
+}
