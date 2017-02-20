15ee8edf9dcb4edff0e20b37155e919228f30b0f
Basim Khajwal <basimkhajwal@gmail.com>
2017-02-20 18:38:05 +0000

    Added an image film implementation

---
+++ src/main/scala/raytracer/films/ImageFilm.scala
@@ -0,0 +1,22 @@
+package raytracer.films
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
+
+import raytracer.Spectrum
+
+/**
+  * Created by Basim on 20/02/2017.
+  */
+class ImageFilm(fileName: String, xRes: Int, yRes: Int) extends Film(xRes, yRes) {
+
+  private val image = new BufferedImage(xRes, yRes, BufferedImage.TYPE_INT_RGB)
+
+  override def applySample(imageX: Int, imageY: Int, l: Spectrum): Unit = {
+    image.setRGB(imageX, imageY, l.toRGBInt)
+  }
+
+  override def saveImage: Unit = {
+    ImageIO.write(image, "png", new File(fileName))
+  }
+}
