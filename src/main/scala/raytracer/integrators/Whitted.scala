4457680928f32f41b1749a9ca52ed76e121abfd6
Basim Khajwal <basimkhajwal@gmail.com>
2017-02-22 16:10:55 +0000

    Added sanity check

--- src/main/scala/raytracer/integrators/Whitted.scala
+++ src/main/scala/raytracer/integrators/Whitted.scala
@@ -32,7 +32,7 @@ class Whitted extends Integrator{
             })
             .foldLeft(Spectrum.BLACK)(_ + _)
 
-        if (depth == options.maxRayDepth) directLight
+        if (depth >= options.maxRayDepth) directLight
         else {
           val newDir = (ray reflect dg.nn).nor
           directLight + bsdf(ray.dir, newDir, BSDFType.ALL_REFLECTION) *traceRay(Ray(p, newDir), depth + 1)
