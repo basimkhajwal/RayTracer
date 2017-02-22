65712111444eed39b792a87b034bdf0a87d9f22b
Basim Khajwal <basimkhajwal@gmail.com>
2017-02-22 16:09:54 +0000

    Used the camera generated by scene builder

--- src/main/scala/raytracer/Renderer.scala
+++ src/main/scala/raytracer/Renderer.scala
@@ -5,21 +5,19 @@ package raytracer
   */
 import java.awt.image.BufferedImage
 
-import raytracer.cameras.{OrthographicCamera, PerspectiveCamera}
+import raytracer.cameras.{Camera, OrthographicCamera, PerspectiveCamera}
 import raytracer.films.Film
 import raytracer.integrators.{Integrator, Whitted}
 import raytracer.math.{Point, Ray, Transform, Vec3}
 
 trait RenderOpts {
-  val maxRayDepth: Int = 4
+  val maxRayDepth: Int = 1
   val pixelSampleCount: Int = 2
 
-  val cameraToWorld: Transform
-
   val scene: Scene
-  val integrator: Integrator = new Whitted()
-
+  val integrator: Integrator
   val film: Film
+  val camera: Camera
 }
 
 class Renderer(options: RenderOpts) {
@@ -27,12 +25,9 @@ class Renderer(options: RenderOpts) {
   def render: Unit = {
     val width = options.film.xResolution
     val height = options.film.yResolution
-    val ar = width.toDouble / height
-    val cam = new PerspectiveCamera(options.cameraToWorld,
-      (-ar, ar, -1, 1), width, height, 0.1, 100, 80)
 
     for (y <- 0 until height; x <- 0 until width) {
-      val lum = options.integrator.traceRay(cam.generateRay(x, y))(options)
+      val lum = options.integrator.traceRay(options.camera.generateRay(x, y))(options)
 
       options.film.applySample(x, y, lum.clamp)
     }
