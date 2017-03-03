8bfd699bbc088cf68ecd4f73606224f9e9f2b44e
Basim Khajwal <basimkhajwal@gmail.com>
2017-03-03 22:01:02 +0000

    Started sampler renderer definition

--- src/main/scala/raytracer/renderers/SamplerRenderer.scala
+++ src/main/scala/raytracer/renderers/SamplerRenderer.scala
@@ -8,10 +8,50 @@ import raytracer.sampling.Sampler
   * Created by Basim on 03/03/2017.
   */
 class SamplerRenderer(
-  val sampler: Sampler,
-  val camera: Camera,
-  val integrator: Integrator
+  sampler: Sampler,
+  camera: Camera,
+  integrator: Integrator,
+  taskCount: Int
 ) extends Renderer {
 
-  override def render(scene: Scene): Unit = ???
+  override def render(scene: Scene): Unit = {
+
+    val subTasks = new Array[Thread](taskCount)
+    var i = 0
+    while (i < taskCount) {
+      subTasks(i) = new Thread(new SamplerRendererTask(sampler, camera, integrator, scene, i, taskCount))
+      subTasks(i).start()
+      i += 1
+    }
+
+    i = 0
+    while (i < taskCount) {
+      subTasks(i).join()
+      i += 1
+    }
+
+    camera.film.saveImage()
+  }
+}
+
+class SamplerRendererTask(
+  mainSampler: Sampler,
+  camera: Camera,
+  integrator: Integrator,
+  scene: Scene,
+  taskIdx: Int,
+  taskCount: Int
+) extends Runnable {
+  val sampler = mainSampler.getSubSampler(taskIdx, taskCount)
+
+  override def run(): Unit = {
+
+    while (!sampler.isFinished()) {
+      val sample = sampler.getNextSample(0,0)
+      val ray = camera.generateRay(sample.imageX, sample.imageY)
+      val li = integrator.traceRay(scene, ray)
+
+      camera.film.applySample(sample.imageX.toInt, sample.imageY.toInt, li)
+    }
+  }
 }
