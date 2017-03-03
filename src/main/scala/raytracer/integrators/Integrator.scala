2f81df77bb8a8f90d6b90663c956ecc79808d349
Basim Khajwal <basimkhajwal@gmail.com>
2017-03-03 22:00:30 +0000

    Removed need for render opts

--- src/main/scala/raytracer/integrators/Integrator.scala
+++ src/main/scala/raytracer/integrators/Integrator.scala
@@ -1,11 +1,11 @@
 package raytracer.integrators
 
 import raytracer.math.Ray
-import raytracer.{RenderOpts, Spectrum}
+import raytracer.{Scene, Spectrum}
 
 /**
   * Created by Basim on 05/01/2017.
   */
 trait Integrator {
-  def traceRay(ray: Ray)(implicit options: RenderOpts): Spectrum
+  def traceRay(scene: Scene, ray: Ray): Spectrum
 }
