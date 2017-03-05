8eb0087fcffbb5029fcb5da7a879bc623cc11812
Basim Khajwal <basimkhajwal@gmail.com>
2017-03-05 19:52:54 +0000

    Added sample extent definition

--- src/main/scala/raytracer/films/Film.scala
+++ src/main/scala/raytracer/films/Film.scala
@@ -7,6 +7,8 @@ import raytracer.Spectrum
   */
 abstract class Film(val xResolution: Int, val yResolution: Int) {
 
+  def getSampleExtent(): (Int, Int, Int, Int) = (0, xResolution-1, 0, yResolution-1)
+
   def applySample(imageX: Int, imageY: Int, l: Spectrum)
 
   def saveImage(): Unit
