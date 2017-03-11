0499ccfe64bfac1322fb293647fbb3561aaebdb4
Basim Khajwal <basimkhajwal@gmail.com>
2017-03-11 22:37:37 +0000

    Added a generic specular reflection method

--- src/main/scala/raytracer/integrators/Integrator.scala
+++ src/main/scala/raytracer/integrators/Integrator.scala
@@ -1,6 +1,8 @@
 package raytracer.integrators
 
+import raytracer.bxdf.BSDF
 import raytracer.math.Ray
+import raytracer.primitives.Intersection
 import raytracer.{Scene, Spectrum}
 
 /**
@@ -8,4 +10,16 @@ import raytracer.{Scene, Spectrum}
   */
 trait Integrator {
   def traceRay(scene: Scene, ray: Ray): Spectrum
+}
+
+object Integrator {
+
+  def specularReflect(scene: Scene, ray: Ray, isect: Intersection, integrator: Integrator): Spectrum = {
+    val dg = isect.dg
+    val bsdf = isect.getBSDF
+    val (col, wi) = bsdf.sample(-ray.dir, 0, 0, BSDF.REFLECTION | BSDF.SPECULAR)
+
+    if (col.isBlack()) col
+    else col * integrator.traceRay(scene, Ray(dg.p, wi.nor, ray.depth+1)) * wi.dot(dg.nn).abs
+  }
 }
