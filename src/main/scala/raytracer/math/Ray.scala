7abfae15dc051f57372df69ec5d2f3b311094688
Basim Khajwal <basimkhajwal@gmail.com>
2017-03-11 22:36:39 +0000

    Added a depth parameter to the ray

--- src/main/scala/raytracer/math/Ray.scala
+++ src/main/scala/raytracer/math/Ray.scala
@@ -5,12 +5,7 @@ import raytracer.Constants._
 /**
   * Created by Basim on 05/01/2017.
   */
-case class Ray(val start: Point, val dir: Vec3) {
+case class Ray(start: Point, dir: Vec3, depth: Int) {
   private val m = dir.mag2
   require(m > 1-EPSILON && m<1+EPSILON, s"Found $m with $start + $dir")
-
-  def reflect(normal: Vec3): Vec3 = {
-    val cosTheta = (-dir).dot(normal)
-    dir + normal*(2*cosTheta)
-  }
 }
