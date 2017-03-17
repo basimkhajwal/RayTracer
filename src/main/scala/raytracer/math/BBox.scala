94d5d36283f2f72d1cc12c74a5219ae36d42a56e
Basim Khajwal <basimkhajwal@gmail.com>
2017-03-17 23:33:11 +0000

    Added intersection checking

--- src/main/scala/raytracer/math/BBox.scala
+++ src/main/scala/raytracer/math/BBox.scala
@@ -34,6 +34,49 @@ case class BBox(pMin: Point, pMax: Point) {
     inRange(point.z, pMin.z, pMax.z)
   }
 
+  def maximumExtent(): Int = {
+    val a = pMax.x - pMin.x
+    val b = pMax.y - pMin.y
+    val c = pMax.z - pMin.z
+    if (a < b) {
+      if (a < c) 0
+      else 2
+    } else {
+      if (b < c) 1
+      else 2
+    }
+  }
+
+  def checkIntersect(ray: Ray): Option[(Double, Double)] = {
+    var i = 0
+    var t0 = 0.0
+    var t1 = Double.PositiveInfinity
+    var valid = true
+
+    while (i < 3) {
+      val invRayDir = 1 / ray.dir(i)
+      var tNear = (pMin(i) - ray.start(i)) * invRayDir
+      var tFar  = (pMax(i) - ray.start(i)) * invRayDir
+
+      if (tNear > tFar) {
+        val temp = tNear
+        tNear = tFar
+        tFar = temp
+      }
+
+      t0 = if (tNear > t0) tNear else t0
+      t1 = if (tFar < t1) tFar else t1
+      if (t0 > t1) {
+        valid = false
+        i = 3
+      }
+
+      i += 1
+    }
+
+    if (valid) Some((t0, t1)) else None
+  }
+
   def union(p: Point): BBox = {
     BBox(
       Point( min(pMin.x, p.x), min(pMin.y, p.y), min(pMin.z, p.z) ),
