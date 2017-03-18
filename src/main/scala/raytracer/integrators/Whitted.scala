82739f1f25ae26dcf5ef0598da64d3b67d86ee53
Basim Khajwal <basimkhajwal@gmail.com>
2017-03-18 18:44:01 +0000

    Used new scene interface

--- src/main/scala/raytracer/integrators/Whitted.scala
+++ src/main/scala/raytracer/integrators/Whitted.scala
@@ -22,19 +22,15 @@ class Whitted(maxRayDepth: Int) extends Integrator{
             val (lightIntensity, wi, lightDist) = l.sample(p)
             val lightValue = lightIntensity * wi.dot(dg.nn) * bsdf(ray.dir, wi, BSDF.ALL_REFLECTION)
 
-            scene intersect(Ray(p, wi, 0), 0, lightDist) match {
-              case None => lightValue
-              case Some(_) => Spectrum.BLACK
+            scene intersect Ray(p, wi, 0) match {
+              case Some(i) if i.time < lightDist => Spectrum.BLACK
+              case _ => lightValue
             }
           })
           .foldLeft(Spectrum.BLACK)(_ + _)
 
         if (ray.depth >= maxRayDepth) directLight
-        else {
-          //val newDir = (ray reflect dg.nn).nor
-          //directLight + bsdf(ray.dir, newDir, BSDF.ALL_REFLECTION) * traceRay(scene, Ray(p, newDir), depth + 1)
-          directLight + Integrator.specularReflect(scene, ray, isect, this)
-        }
+        else directLight + Integrator.specularReflect(scene, ray, isect, this)
       }
     }
   }
