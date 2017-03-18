2f627fde8918d62e953283c9c7c24e13d12f3730
Basim Khajwal <basimkhajwal@gmail.com>
2017-03-18 19:18:19 +0000

    Removed intersection definition

--- src/main/scala/raytracer/Scene.scala
+++ src/main/scala/raytracer/Scene.scala
@@ -7,18 +7,6 @@ import raytracer.primitives.{Intersection, Primitive}
 /**
   * Created by Basim on 18/12/2016.
   */
-class Scene(val lights: List[Light], val objects: List[Primitive]) {
-
-  def intersect(ray: Ray, minT: Double = 0, maxT: Double = Double.PositiveInfinity): Option[Intersection] = {
-    val intersections = objects
-      .map(_.intersect(ray)
-            .flatMap(i => if (i.time > maxT || i.time < minT) None else Some(i)))
-
-    if (intersections.isEmpty) None
-    else
-      intersections.minBy(_ match {
-        case None => Double.PositiveInfinity
-        case Some(Intersection(_, _, t)) => t
-      })
-  }
+class Scene(val lights: List[Light], val primitive: Primitive) {
+  def intersect(ray: Ray): Option[Intersection] = primitive.intersect(ray)
 }
