c8d7dbf35c106f9c9e6c51a51337dd3d04113813
Basim Khajwal <basimkhajwal@gmail.com>
2017-03-21 23:02:51 +0000

    Added linear 2x2 matrix solver

---
+++ src/main/scala/raytracer/math/Solver.scala
@@ -0,0 +1,20 @@
+package raytracer.math
+
+/**
+  * Created by Basim on 21/03/2017.
+  */
+object Solver {
+
+  val EPSILON = 1e-9
+
+  def linearEquation(mat: Array[Double], y0: Double, y1: Double): Option[(Double, Double)] = {
+    val det = mat(0)*mat(3)-mat(1)*mat(2)
+    if (det < EPSILON) return None
+
+    val x0 = (mat(3)*y0 - mat(1)*y1) / det
+    val x1 = (mat(0)*y0 - mat(2)*y1) / det
+
+    if (x0 == Double.NaN || x1 == Double.NaN) None
+    else Some(x0, x1)
+  }
+}
