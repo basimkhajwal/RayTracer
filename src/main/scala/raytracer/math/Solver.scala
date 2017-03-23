5f7f09fe1e26a397b019e9be5555bca71f8566f0
Basim Khajwal <basimkhajwal@gmail.com>
2017-03-23 22:54:10 +0000

    Corrected mistakes

--- src/main/scala/raytracer/math/Solver.scala
+++ src/main/scala/raytracer/math/Solver.scala
@@ -9,10 +9,10 @@ object Solver {
 
   def linearEquation(mat: Array[Double], y0: Double, y1: Double): Option[(Double, Double)] = {
     val det = mat(0)*mat(3)-mat(1)*mat(2)
-    if (det < EPSILON) return None
+    if (math.abs(det) < EPSILON) return None
 
     val x0 = (mat(3)*y0 - mat(1)*y1) / det
-    val x1 = (mat(0)*y0 - mat(2)*y1) / det
+    val x1 = (mat(0)*y1 - mat(2)*y0) / det
 
     if (x0 == Double.NaN || x1 == Double.NaN) None
     else Some(x0, x1)
