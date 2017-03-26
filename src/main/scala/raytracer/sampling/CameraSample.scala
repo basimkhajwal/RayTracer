157234a70f8c1daf25bc8d6166d10220c7316fa9
Basim Khajwal <basimkhajwal@gmail.com>
2017-03-26 22:51:53 +0100

    Added lens parameters

--- src/main/scala/raytracer/sampling/CameraSample.scala
+++ src/main/scala/raytracer/sampling/CameraSample.scala
@@ -4,6 +4,7 @@ package raytracer.sampling
   * Created by Basim on 02/03/2017.
   */
 case class CameraSample (
-  val imageX: Double, val imageY: Double
+  imageX: Double, imageY: Double,
+  lensU: Double, lensV: Double
 )
 
--- src/main/scala/raytracer/sampling/Sample.scala
+++ src/main/scala/raytracer/sampling/Sample.scala
@@ -4,13 +4,13 @@ package raytracer.sampling
   * Created by Basim on 02/03/2017.
   */
 class Sample(
- iX: Double, iY: Double,
+ iX: Double, iY: Double, lX: Double, lY: Double,
  val oneD: Array[Double], val twoD: Array[Double]
-) extends CameraSample(iX, iY)
+) extends CameraSample(iX, iY, lX, lY)
 
 object Sample {
-  def apply(iX: Double, iY: Double,
+  def apply(iX: Double, iY: Double, lX: Double, lY: Double,
     oneD: Array[Double], twoD: Array[Double]
-  ): Sample = new Sample(iX, iY, oneD, twoD)
+  ): Sample = new Sample(iX, iY, lX, lY, oneD, twoD)
 }
 
