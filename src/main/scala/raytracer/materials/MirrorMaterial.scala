19a6ee6801a3d918a5bd62eef8d54fb59eb30ef4
Basim Khajwal <basimkhajwal@gmail.com>
2017-03-29 23:29:24 +0100

    Added simple mirror material

---
+++ src/main/scala/raytracer/materials/MirrorMaterial.scala
@@ -0,0 +1,23 @@
+package raytracer.materials
+
+import raytracer.Spectrum
+import raytracer.bxdf.{BSDF, SpecularReflection}
+import raytracer.shapes.DifferentialGeometry
+import raytracer.textures.Texture
+
+/**
+  * Created by Basim on 29/03/2017.
+  */
+class MirrorMaterial (
+  val r: Texture[Spectrum]
+) extends Material {
+
+  override def getBSDF(dgGeom: DifferentialGeometry, dgShading: DifferentialGeometry): BSDF = {
+    val bsdf = new BSDF(dgShading, dgGeom.nn)
+
+    val colour = r(dgShading).clamp
+    if (!colour.isBlack()) bsdf.add(new SpecularReflection(colour))
+
+    bsdf
+  }
+}
