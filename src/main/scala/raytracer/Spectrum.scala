810c8eea05e4c1ef5117a616d8bb9a2dbd7992ab
Basim Khajwal <basimkhajwal@gmail.com>
2017-03-30 21:10:18 +0100

    Added more operator overloading for spectra

--- src/main/scala/raytracer/Spectrum.scala
+++ src/main/scala/raytracer/Spectrum.scala
@@ -14,8 +14,18 @@ case class Spectrum(r: Double, g: Double, b: Double) {
   def toRGBInt: Int = (b*255.0).toInt + ((g*255.0).toInt << 8) + ((r*255.0).toInt << 16)
 
   def +(that: Spectrum): Spectrum = Spectrum(r + that.r, g + that.g, b + that.b)
+
+  def +(that: Double): Spectrum = Spectrum(r + that, g + that, b + that)
+
+  def -(that: Spectrum): Spectrum = Spectrum(r - that.r, g - that.g, b - that.b)
+
   def *(sf: Double): Spectrum = Spectrum(r * sf, g * sf, b * sf)
+
+  def /(sf: Double): Spectrum = this * (1 / sf)
+
   def *(that: Spectrum): Spectrum = Spectrum(r * that.r, g * that.g, b * that.b)
+
+  def /(that: Spectrum): Spectrum = Spectrum(r / that.r, g / that.g, b / that.b)
 
   def clamp: Spectrum = Spectrum(Math.min(r, 1), Math.min(g, 1), Math.min(b, 1))
 
