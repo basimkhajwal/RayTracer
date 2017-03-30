1ac9777eadc43c538aaa6792a0aa280ebf063db0
Basim Khajwal <basimkhajwal@gmail.com>
2017-03-30 22:06:46 +0100

    More testing

--- src/main/scala/raytracer/materials/PlasticMaterial.scala
+++ src/main/scala/raytracer/materials/PlasticMaterial.scala
@@ -1,7 +1,7 @@
 package raytracer.materials
 
 import raytracer.Spectrum
-import raytracer.bxdf.BSDF
+import raytracer.bxdf._
 import raytracer.shapes.DifferentialGeometry
 import raytracer.textures.Texture
 
@@ -11,14 +11,20 @@ import raytracer.textures.Texture
 class PlasticMaterial(
   val kd: Texture[Spectrum],
   val ks: Texture[Spectrum],
-  val rough: Texture[Float]
+  val rough: Texture[Double]
   /* bump: Texture[Float] */
 ) extends Material {
+
+  val fresnel = new FresnelNoOp() //new FresnelDielectric(1.5, 1)
 
   override def getBSDF(dgGeom: DifferentialGeometry, dgShading: DifferentialGeometry): BSDF = {
     val bsdf = new BSDF(dgShading, dgGeom.nn)
 
-    // TODO: Add fresnel for Microfacet specular reflection
+    val d = kd(dgShading).clamp
+    val s = ks(dgShading).clamp
+
+    bsdf.add(new Lambertian(d))
+    bsdf.add(new SpecularReflection(s, fresnel))
 
     bsdf
   }
