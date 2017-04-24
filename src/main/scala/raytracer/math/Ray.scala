a8d551d6d2f0a2de6f49d87fdc5200f16e075406
Basim Khajwal <basimkhajwal@gmail.com>
2017-04-24 22:27:29 +0100

    Added ray differentials, to be implemented through the project at a later date

--- src/main/scala/raytracer/math/Ray.scala
+++ src/main/scala/raytracer/math/Ray.scala
@@ -3,7 +3,24 @@ package raytracer.math
 /**
   * Created by Basim on 05/01/2017.
   */
-case class Ray(start: Point, dir: Vec3, depth: Int) {
-  private val m = dir.mag2
-  require(m > 1-1e-9 && m<1+1e-9, s"Found $m with $start + $dir")
+case class Ray(start: Point, dir: Vec3, depth: Int)
+
+class RayDifferential(
+  start: Point, dir: Vec3, depth: Int,
+  val rxOrigin: Point, val ryOrigin: Point,
+  val rxDirection: Vec3, val ryDirection: Vec3
+) extends Ray(start, dir, depth)
+
+object RayDifferential {
+  def apply(
+    start: Point, dir: Vec3, depth: Int,
+    rxOrigin: Point, ryOrigin: Point,
+    rxDirection: Vec3, ryDirection: Vec3
+  ): RayDifferential = {
+    new RayDifferential(
+      start, dir, depth,
+      rxOrigin, ryOrigin,
+      rxDirection, ryDirection
+    )
+  }
 }
