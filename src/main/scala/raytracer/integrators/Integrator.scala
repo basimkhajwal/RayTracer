c84ffbb2acf8799e17e5e85d66d1eb724ecdd45d
Basim Khajwal <basimkhajwal@gmail.com>
2017-06-11 14:46:51 +0100

    Updated to guard against 0 pdfs

--- src/main/scala/raytracer/integrators/Integrator.scala
+++ src/main/scala/raytracer/integrators/Integrator.scala
@@ -17,9 +17,11 @@ object Integrator {
   def specularReflect(scene: Scene, ray: Ray, isect: Intersection, integrator: Integrator): Spectrum = {
     val dg = isect.dg
     val bsdf = isect.getBSDF()
-    val (col, wi) = bsdf.sample(-ray.dir, 0, 0, BSDF.REFLECTION | BSDF.SPECULAR)
-    val mask = col * wi.dot(dg.nn).abs
+    val (col, wi, pdf) = bsdf.sample(-ray.dir, 0, 0, BSDF.REFLECTION | BSDF.SPECULAR)
 
+    if (pdf < 1e-9) return Spectrum.BLACK
+
+    val mask = col * (wi.dot(dg.nn).abs / pdf)
     if (mask.isBlack()) mask
     else mask.clamp() * integrator.traceRay(scene, Ray(dg.p + wi*1e-9, wi, ray.depth+1))
   }
@@ -27,8 +29,10 @@ object Integrator {
   def specularTransmit(scene: Scene, ray: Ray, isect: Intersection, integrator: Integrator): Spectrum = {
     val dg = isect.dg
     val bsdf = isect.getBSDF()
-    val (col, wi) = bsdf.sample(-ray.dir, 0, 0, BSDF.TRANSMISSION | BSDF.SPECULAR)
-    val mask = col * wi.dot(dg.nn).abs
+    val (col, wi, pdf) = bsdf.sample(-ray.dir, 0, 0, BSDF.TRANSMISSION | BSDF.SPECULAR)
+    val mask = col * (wi.dot(dg.nn).abs / pdf)
+
+    if (pdf < 1e-9) return Spectrum.BLACK
 
     if (mask.isBlack()) mask
     else mask.clamp() * integrator.traceRay(scene, Ray(dg.p + wi*1e-9, wi, ray.depth+1))
